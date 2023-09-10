b4d8fc7afabff7fa82f7690b3b356fe0e90c0062
MOMAN11 <thoughtworks.mo@extaccount.com>
2023-09-10 16:45:22 +0800

    add addbook modal

--- src/dashboard/bookSupplementation/actions/useBookSupplementation.js
+++ src/dashboard/bookSupplementation/actions/useBookSupplementation.js
@@ -1,7 +1,32 @@
 const useBookSupplementation = () => {
 
     const addBook = () => {
-
+        // const reqBody = {
+        //     "title": "In Search of Lost Time",
+        //     "author": "Prust",
+        //     "category":"Stream of Consciousness Novel",
+        //     "publisher": "Heaven"
+        // };
+        // fetch("api/books", {
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //     },
+        //     method: "post",
+        //     body: JSON.stringify(reqBody)
+        // })
+        //     .then(response => {
+        //         if (response.status === 200) {
+        //             return Promise.all([response.json()]);
+        //         } else {
+        //             return Promise.reject("Wrong username or password");
+        //         }
+        //     })
+        //     .then(([body]) => {
+        //         user.setJwt(body.access_token);
+        //         user.setRefreshJwt(body.refresh_token);
+        //     }).catch((message) => {
+        //     setErrorMessage(message);
+        // });
     }
     return {addBook}
 }
--- src/dashboard/bookSupplementation/components/AddBookModal.js
+++ src/dashboard/bookSupplementation/components/AddBookModal.js
@@ -1,9 +1,63 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import {Col, Row} from "react-bootstrap";
 
 const AddBookModal = () => {
     return (
         <div>
-            
+            <Modal
+                show={true}
+                size="lg"
+                centered
+            >
+                <Modal.Header className="d-flex justify-content-center">
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Add a New Book Here!
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextTitle">
+                            <Form.Label column sm="2">
+                                Title
+                            </Form.Label>
+                            <Col sm="10">
+                                <Form.Control type="title" placeholder="Title" />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextAuthor">
+                            <Form.Label column sm="2">
+                                Author
+                            </Form.Label>
+                            <Col sm="10">
+                                <Form.Control type="author" placeholder="Author" />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextCategory">
+                            <Form.Label column sm="2">
+                                Category
+                            </Form.Label>
+                            <Col sm="10">
+                                <Form.Control type="category" placeholder="Category" />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextPublisher">
+                            <Form.Label column sm="2">
+                                Publisher
+                            </Form.Label>
+                            <Col sm="10">
+                                <Form.Control type="publisher" placeholder="Publisher" />
+                            </Col>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary">Close</Button>
+                    <Button variant="primary">Submit</Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 };
