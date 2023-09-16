b3633688f6ad90f519a794badca81c69832e9952
MOMAN11 <thoughtworks.mo@extaccount.com>
2023-09-16 15:54:33 +0800

    extract validate and refresh jwt

---
+++ src/authentication/useValidateAndRefreshJwt.js
@@ -0,0 +1,42 @@
+import useRoles from "./useRoles";
+import {useNavigate} from "react-router-dom";
+
+const useValidateAndRefreshJwt = () => {
+    const {user, decodedJwt} = useRoles();
+    const navigate = useNavigate();
+
+    const validateAndRefreshJwt = () => {
+        console.log("start to invoke useEffect method!========")
+        if (decodedJwt.sub === "" || decodedJwt.roles.length === 0) {
+            navigate("/login");
+        } else if (decodedJwt.exp < Date.now() / 1000) {
+            fetch("api/auth/token/refresh", {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + user.refreshJwt
+                },
+                method: "get"
+            }).then(res => {
+                if (res.status === 200) {
+                    return res.json();
+                } else {
+                    return Promise.reject(res)
+                }
+            })
+                .then(data => {
+                    user.setJwt(data.access_token);
+                })
+                .catch(error => {
+                    error.json().then(data => {
+                        navigate("/login");
+                    })
+                });
+        }
+    }
+
+
+    return {validateAndRefreshJwt, user}
+};
+
+export default useValidateAndRefreshJwt
+
