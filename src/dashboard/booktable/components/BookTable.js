0b3c68c36e46834e3351d7a3b3f1d88eb7c6ad42
MOMAN11 <thoughtworks.mo@extaccount.com>
2023-09-17 21:17:36 +0800

    adjust pagination position

--- src/dashboard/booktable/components/BookTable.js
+++ src/dashboard/booktable/components/BookTable.js
@@ -1,5 +1,5 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {Col, Pagination, Row, Table} from "react-bootstrap";
+import {Col, Container, Pagination, Row, Table} from "react-bootstrap";
 import useUser from "../../../authentication/useUser";
 import {BannerContext} from "../../../banner/BannerProvider";
 import Banner from "../../../banner/Banner";
@@ -82,48 +82,52 @@ const BookTable = ({setBooks, books}) => {
                 })}
                 </tbody>
             </Table>
-            <Row>
-                <Col className="d-flex justify-content-center">
-                    {pageInfo.lastPage < 10 ? <Pagination>
-                        {[...Array(pageInfo.lastPage).keys()].map(
-                            pageNumber =>
-                                <Pagination.Item key={pageNumber}
-                                                 onClick={() => handlePageClick(pageNumber + 1)}
-                                                 active={pageNumber + 1 === pageInfo.currentPage}>{pageNumber + 1}
-                                </Pagination.Item>
-                        )}
-                    </Pagination> : <Pagination>
-                        <Pagination.First
-                            onClick={() => handlePageClick(pageInfo.firstPage)}
-                        />
-                        <Pagination.Prev
-                            onClick={() => handlePageClick(pageInfo.currentPage - 1)}
-                            disabled={pageInfo.currentPage <= pageInfo.firstPage}
-                        />
-                        <Pagination.Item
-                            onClick={() => handlePageClick(pageInfo.firstPage)}
-                            active={pageInfo.firstPage === pageInfo.currentPage}>{pageInfo.firstPage}</Pagination.Item>
-                        <Pagination.Ellipsis disabled/>
-                        <Pagination.Item
-                            onClick={() => handlePageClick(pageInfo.middlePage)}
-                            active={pageInfo.middlePage === pageInfo.currentPage}>{pageInfo.middlePage}</Pagination.Item>
-                        <Pagination.Ellipsis disabled/>
-                        <Pagination.Item
-                            onClick={() => handlePageClick(pageInfo.lastPage)}
-                            active={pageInfo.lastPage === pageInfo.currentPage}>{pageInfo.lastPage}</Pagination.Item>
-                        <Pagination.Next
-                            onClick={() => handlePageClick(pageInfo.currentPage + 1)}
-                            disabled={pageInfo.currentPage >= pageInfo.lastPage}
-                        />
-                        <Pagination.Last
-                            onClick={() => handlePageClick(pageInfo.lastPage)}
-                        />
-                    </Pagination>}
-                </Col>
-            </Row>
-            <Row className="d-flex justify-content-center">
-                {pageInfo.pageSize} books/page; total: {pageInfo.totalElements} books
-            </Row>
+            <Container className="fixed-footer">
+                <Row>
+                    <Col className="d-flex justify-content-center">
+                        {pageInfo.lastPage < 10 ? <Pagination>
+                            {[...Array(pageInfo.lastPage).keys()].map(
+                                pageNumber =>
+                                    <Pagination.Item key={pageNumber}
+                                                     onClick={() => handlePageClick(pageNumber + 1)}
+                                                     active={pageNumber + 1 === pageInfo.currentPage}>{pageNumber + 1}
+                                    </Pagination.Item>
+                            )}
+                        </Pagination> : <Pagination>
+                            <Pagination.First
+                                onClick={() => handlePageClick(pageInfo.firstPage)}
+                            />
+                            <Pagination.Prev
+                                onClick={() => handlePageClick(pageInfo.currentPage - 1)}
+                                disabled={pageInfo.currentPage <= pageInfo.firstPage}
+                            />
+                            <Pagination.Item
+                                onClick={() => handlePageClick(pageInfo.firstPage)}
+                                active={pageInfo.firstPage === pageInfo.currentPage}>{pageInfo.firstPage}</Pagination.Item>
+                            <Pagination.Ellipsis disabled/>
+                            <Pagination.Item
+                                onClick={() => handlePageClick(pageInfo.middlePage)}
+                                active={pageInfo.middlePage === pageInfo.currentPage}>{pageInfo.middlePage}</Pagination.Item>
+                            <Pagination.Ellipsis disabled/>
+                            <Pagination.Item
+                                onClick={() => handlePageClick(pageInfo.lastPage)}
+                                active={pageInfo.lastPage === pageInfo.currentPage}>{pageInfo.lastPage}</Pagination.Item>
+                            <Pagination.Next
+                                onClick={() => handlePageClick(pageInfo.currentPage + 1)}
+                                disabled={pageInfo.currentPage >= pageInfo.lastPage}
+                            />
+                            <Pagination.Last
+                                onClick={() => handlePageClick(pageInfo.lastPage)}
+                            />
+                        </Pagination>}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="d-flex justify-content-center">
+                        {pageInfo.pageSize} books/page; total: {pageInfo.totalElements} books
+                    </Col>
+                </Row>
+            </Container>
         </Banner>
 
     );
