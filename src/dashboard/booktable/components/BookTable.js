cf16bb04d10c46cc812240c86840f5db79c86168
MOMAN11 <thoughtworks.mo@extaccount.com>
2023-09-17 17:36:24 +0800

    add pagination

--- src/dashboard/booktable/components/BookTable.js
+++ src/dashboard/booktable/components/BookTable.js
@@ -1,5 +1,5 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {Table} from "react-bootstrap";
+import {Col, Pagination, Row, Table} from "react-bootstrap";
 import useUser from "../../../authentication/useUser";
 import {BannerContext} from "../../../banner/BannerProvider";
 import Banner from "../../../banner/Banner";
@@ -9,6 +9,12 @@ const BookTable = ({setBooks, books}) => {
     const user = useUser();
     const {setBannerStyle, setBannerMessage, bannerMessage, bannerStyle} = useContext(BannerContext);
     const [isShowBookTableErrorBanner, setIsShowBookTableErrorBanner] = useState(false);
+    const [pageInfo, setPageInfo] = useState({
+        "firstPage": 1,
+        "lastPage": null,
+        "totalElements": null,
+        "pageSize": null
+    })
 
     const fetchBooks = () => {
         fetch("api/admin/books", {
@@ -20,15 +26,23 @@ const BookTable = ({setBooks, books}) => {
             .then((response) =>
                 response.json()
             )
-            .then(data => setBooks(data.content))
+            .then(data => {
+                setBooks(data.content);
+                setPageInfo({
+                    ...pageInfo,
+                    "lastPage": data.totalPages,
+                    "totalElements": data.totalElements,
+                    "pageSize": data.size,
+                })
+            })
             .catch(() => {
                 setIsShowBookTableErrorBanner(true);
                 setBannerStyle('danger');
                 setBannerMessage("The server has some error. Please try again");
-        })
+            })
     }
 
-    useEffect(fetchBooks, [setBooks, user.jwt]);
+    useEffect(fetchBooks, [setBannerMessage, setBannerStyle, setBooks, user.jwt]);
 
 
     return (
@@ -54,9 +68,32 @@ const BookTable = ({setBooks, books}) => {
                             <td>{book.category}</td>
                             <td>{book.publisher}</td>
                         </tr>
-                    )})}
+                    )
+                })}
                 </tbody>
             </Table>
+            <Row>
+                <Col className="d-flex justify-content-center">
+                    {pageInfo.lastPage < 10 ? <Pagination>
+                        {[...Array(pageInfo.lastPage).keys()].map(
+                            pageNumber => <Pagination.Item>{pageNumber + 1}</Pagination.Item>
+                        )}
+                    </Pagination> : <Pagination>
+                        <Pagination.First/>
+                        <Pagination.Prev/>
+                        <Pagination.Item>{pageInfo.firstPage}</Pagination.Item>
+                        <Pagination.Ellipsis/>
+                        <Pagination.Item>{Math.floor((pageInfo.firstPage + pageInfo.lastPage) / 2)}</Pagination.Item>
+                        <Pagination.Ellipsis/>
+                        <Pagination.Item>{pageInfo.lastPage}</Pagination.Item>
+                        <Pagination.Next/>
+                        <Pagination.Last/>
+                    </Pagination>}
+                </Col>
+            </Row>
+            <Row className="d-flex justify-content-center">
+                {pageInfo.pageSize} books/page; total: {pageInfo.totalElements} books
+            </Row>
         </Banner>
 
     );
