95b286e50282bf0ec194497774717d22b9cbb469
MOMAN11 <thoughtworks.mo@extaccount.com>
2023-10-01 15:57:36 +0800

    change banner text style

--- src/banner/Banner.js
+++ src/banner/Banner.js
@@ -9,7 +9,7 @@ const Banner = ({isShowErrorBanner, isShowSuccessBanner, bannerStyle, bannerMess
                 <Row>
                     <Col>
                         <Alert key={bannerStyle} variant={bannerStyle} show={isShowErrorBanner}
-                               style={{height: "40px", marginBottom: "0px", padding: "0px", color: "gray"}}
+                               style={{height: "40px", marginBottom: "0px", padding: "0px"}}
                                className="d-flex align-items-center justify-content-center">
                             <strong>{bannerMessage}</strong>
                         </Alert>
@@ -21,7 +21,7 @@ const Banner = ({isShowErrorBanner, isShowSuccessBanner, bannerStyle, bannerMess
                 <Row>
                     <Col>
                         <Alert key={bannerStyle} variant={bannerStyle} show={isShowSuccessBanner}
-                               style={{height: "40px", marginBottom: "0px", padding: "0px", color: "gray"}}
+                               style={{height: "40px", marginBottom: "0px", padding: "0px"}}
                                className="d-flex align-items-center justify-content-center">
                             <strong>{bannerMessage}</strong>
                         </Alert>
