8dfde5d1d1ca5edeb6e318b83af4159ecacd0406
shooka <manmo431@gmail.com>
2023-10-15 13:15:49 +0800

    Frontend - refactor PrivateRoute component

--- src/authentication/PrivateRoute.js
+++ src/authentication/PrivateRoute.js
@@ -7,25 +7,23 @@ const PrivateRoute = ({children}) => {
     const { validateAndRefreshJwt, jwt } = useContext(UserContext);
     const navigate = useNavigate();
     const [isValid, setIsValid] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
-    const [isLogOutLoading, setIsLogOutLoading] = useState(true);
+    const [isJwtValidLoading, setIsJwtValidLoading] = useState(true);
+    const [isJwtInvalidLoading, setIsJwtInvalidLoading] = useState(true);
     const isLogOut = !jwt;
     useEffect(() => {
         validateAndRefreshJwt().then((data) => {
-            if (data !== "") {
-                console.log("data is valid==============", data);
-                setIsValid(true);
-                setIsLoading(false);
-            } else {
-                console.log("data is invalid==============", data);
+            if (data === "") {
                 setIsValid(false);
-                setIsLogOutLoading(false);
+                setIsJwtInvalidLoading(false);
                 navigate("/login");
+            } else {
+                setIsValid(true);
+                setIsJwtValidLoading(false);
             }
         });
     }, [validateAndRefreshJwt, navigate]);
 
-    if (isLoading || (isLogOut && isLogOutLoading)) {
+    if (isJwtValidLoading || (isLogOut && isJwtInvalidLoading)) {
         return <div>Loading...</div>;
     } else if (isValid) {
         return children;
